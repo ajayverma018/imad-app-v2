2cbfd8f5d7e108519ec44b24625b65ffdd51c34d
ajayverma018 <ajayverma018@gmail.com>
2017-02-15 12:05:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
-console.log('Loaded!');
+console.log('Loaded! yupeeeee');
+var element= get.elementById("txt");
+element.innerHTML="hi this is ajay ";
+
+//move image
+var img= get.elementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft= marginLeft + 10;
+    img.style.marginLeft= marginLeft + "px";
+}
+img.onclick = function (){
+    var interval = setInterval(moveRight,100 );
+} ;
